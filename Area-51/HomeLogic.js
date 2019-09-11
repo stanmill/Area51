63b0e2e3f1d3b92b0971dcbe76d3496c0079c289
stanmill <stanley.pena29@gmail.com>
2019-09-11 16:37:46 -0400

    added a graph

--- Area-51/HomeLogic.js
+++ Area-51/HomeLogic.js
@@ -24,4 +24,32 @@ let x = setInterval(function(){
         document.getElementById("minute").innerHTML ='00' ;  
         document.getElementById("second").innerHTML = '00';
     }
-},1000); 
+},1000);
+
+window.onload = function() {
+
+    var chart = new CanvasJS.Chart("chartContainer", {
+        theme: "dark2", // "light1", "light2", "dark1", "dark2"
+        exportEnabled: true,
+        animationEnabled: true,
+        title: {
+            text: "Fake Alien Related News Chart"
+        },
+        data: [{
+            type: "pie",
+            startAngle: 25,
+            toolTipContent: "<b>{label}</b>: {y}%",
+            showInLegend: "true",
+            legendText: "{label}",
+            indexLabelFontSize: 16,
+            indexLabel: "{label} - {y}%",
+            dataPoints: [
+                { y: 90.00, label: "Fake News" },
+                { y: 4.00, label: "Real News" },
+                { y: 6.00, label: "Hard To Tell News" }, 
+            ]
+        }]
+    });
+    chart.render();
+    
+}
