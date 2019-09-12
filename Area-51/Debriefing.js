00badc3becd93efedfdabb74cdc2875156b18d0d
ValorWind1 <skyjdani@gmail.com>
2019-09-12 08:51:53 -0400

    "Dan 1.6"

---
+++ Area-51/Debriefing.js
@@ -0,0 +1,4 @@
+function firstFunc(){
+
+  document.getElementById("fuc1").innerHTML = "On that day, mankind received a grim reminder. We lived in fear of the Aliens from Florida and were disgraced to live in these cages we called walls.";
+}
