c09aa9e9afe7028c0e8b9621f7bc2775df5f8f65
stanmill <stanley.pena29@gmail.com>
2019-09-24 15:57:25 -0400

    backend services

---
+++ Area-51/API/MaterialApi/src/main/java/com/collabera/material/MaterialApiApplication.java
@@ -0,0 +1,13 @@
+package com.collabera.material;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialApiApplication.class, args);
+	}
+
+}
---
+++ Area-51/API/MaterialApi/src/test/java/com/collabera/material/MaterialApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.collabera.material;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MaterialApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
