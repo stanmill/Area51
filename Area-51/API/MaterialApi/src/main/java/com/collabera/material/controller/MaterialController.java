818ee5fd1b3cdbb4da51ae6584f36ce3f8961390
stanmill <stanley.pena29@gmail.com>
2019-09-25 15:27:05 -0400

    Update MaterialController.java

--- Area-51/API/MaterialApi/src/main/java/com/collabera/material/controller/MaterialController.java
+++ Area-51/API/MaterialApi/src/main/java/com/collabera/material/controller/MaterialController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
